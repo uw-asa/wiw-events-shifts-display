2de2ff0c28220bdc9f3ef0ca69a2e79cd583df5d
Chase Sawyer <shadowimmage@users.noreply.github.com>
2021-10-13 14:50:39 -0700

    Updated Electron to v12.x; Major logic restructuring
    Updated production dependencies to the latest versions available and confirmed that display is working on real device.
    Architecture of application logic was changed around to accommodate the changes made to how electron exposes the Node APIs to the renderer process (it doesn't expose anything unless explicitly passed through.
    Made other minor changes to aux files to patch up some functionality lost in the Electron update.
    Fixed issues with .dockerignore syntax (it's different from .gitignore's)
    
    Closes #22

--- renderer.js
+++ renderer.js
@@ -1,133 +1,29 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-const moment = require('moment');
-const pug = require('pug');
 
-// always rendered elements
-const clockTemplate = pug.compileFile('./templates/clock.pug');
+// Pull in environment variables exposed by preload
+const {
+  DISPLAY_CORNER_TITLE,
+  MAX_REFRESH_INTERVAL,
+} = window.preload.envVars;
+
 // set corner title
-document.getElementById('title').firstElementChild.innerHTML = process.env.DISPLAY_CORNER_TITLE || 'Events Display';
+document.getElementById('title').firstElementChild.innerHTML = DISPLAY_CORNER_TITLE || 'Events Display';
 
-// pull utilities into renderer scope
-const { getWiwSchedule } = require('./utils/fetchWiwSchedule');
-const getEmsSchedule = require('./utils/fetchEmsSchedule');
-const markupEventShiftResults = require('./utils/renderEventShifts');
-const markupLaborResults = require('./utils/renderLaborCondensed');
-const markupEventListResults = require('./utils/renderEventList');
+// pull in IPC function references
+const {
+  updateApiContent,
+} = window.preload.ipc;
 
-// main template global
-let renderModeSingle = true;
+// pull template processors
+const {
+  clockTemplate,
+} = window.preload.templates;
 
-// Configure rendering mode and constants for control:
-// options for DISPLAY_RENDER_MODEs: "LABOR", "EVENTS", or "EMS-EVENTS"
-// LABOR = When I Work Labor (condensed view)
-// EVENTS = When I Work labor events associated with an event
-// EMS-EVENTS = Events scheduled in EMS (regardless of labor assignment)
-const ACCEPTABLE_SETTINGS = ['LABOR', 'EVENTS', 'EMS-EVENTS'];
-const SETTINGS_DATA_MAPPING = {
-  LABOR: 'WIW',
-  EVENTS: 'WIW',
-  'EMS-EVENTS': 'EMS',
-};
-const renderModeSettingLeft = process.env.DISPLAY_RENDER_MODE_LEFT;
-const renderModeSettingRight = process.env.DISPLAY_RENDER_MODE_RIGHT;
-const dualSourceTemplate = pug.compileFile('./templates/body2column-DualSource.pug');
-const singleSourceTemplate = pug.compileFile('./templates/body2column-SingleSource.pug');
-if (renderModeSettingLeft !== renderModeSettingRight) {
-  renderModeSingle = false;
-}
-if (!ACCEPTABLE_SETTINGS.includes(renderModeSettingLeft) || !ACCEPTABLE_SETTINGS.includes(renderModeSettingRight)) {
-  throw new Error(`IMPROPER CONFIGURATION; RENDER MODE NOT SET TO ONE OF ${ACCEPTABLE_SETTINGS.toString()}`);
-}
-
-/**
- * Uses the passed data function to gather data. Handles a couple of error conditions.
- *
- * @param {*} dataFn - Data handling function to call to gather data. Data function
- * should do some preprocessing of the initial data returned from that specific API.
- * @returns - result.data payload from data function.
- */
-function getDataFromEndpoint(dataFn) {
-  return new Promise((resolve, reject) => {
-    dataFn().then((result) => {
-      if (result.status === 200) {
-        resolve(result.data);
-      } else {
-        // axios success, but non-200 status code
-        reject(new Error('Received non-200 status'));
-      }
-    })
-      .catch((e) => {
-        // Axios request failure / promise rejection handling
-        reject(e);
-      });
-  });
-}
-
-
-/** Function handles calling and marking up data from given data source.
- * Accepts a data fetching function and a markup function. Used when rendering single-mode.
- *
- * @param {function} dataFn - data fetch and initial processing function
- * @param {function} markupFn - template to use on each piece of data
- * @returns {Promise} marked up HTML on success
- */
-function renderDataHtmlSingle(dataFn, markupFn) {
-  return new Promise((resolve, reject) => {
-    getDataFromEndpoint(dataFn).then((data) => {
-      resolve(singleSourceTemplate({
-        content: markupFn(data).join(''),
-      }));
-    }).catch((e) => reject(e));
-  });
-}
-
-/**
- * Function handles dual data source rendering. Uses given data function and markup functions
- * to fire off asynchronous fetches and then renders the resulting data into a single HTML
- * block using the dual rendering template.
- *
- * @param {*} lDataFn - Data fetching function to gather information for the left column
- * @param {*} lMarkupFn - Data markup function for left column cards using data from lDataFn
- * @param {*} rDataFn - Data fetching function to gather information for the right column
- * @param {*} rMarkupFn - Data markup function for right column cards using data from rDataFn
- * @returns {Promise} Promise that will resolve with the combined HTML content to be injected
- *  into the main content of the display
- */
-function renderDataHtmlDual(lDataFn, lMarkupFn, rDataFn, rMarkupFn) {
-  const lResult = getDataFromEndpoint(lDataFn);
-  const rResult = getDataFromEndpoint(rDataFn);
-  return Promise.all([lResult, rResult]).then((values) => dualSourceTemplate({
-    contentLeft: lMarkupFn(values[0]).join(''),
-    leftHeader: process.env.DISPLAY_LEFT_COLUMN_TITLE,
-    contentRight: rMarkupFn(values[1]).join(''),
-    rightHeader: process.env.DISPLAY_RIGHT_COLUMN_TITLE,
-  }));
-}
-
-/**
- * Helper function that uses configuration options to determine which functions to use
- * for data gathering and markup of that resulting data.
- *
- * @param {String} modeSetting - the mode setting pulled from Config Vars
- * @returns [{dataFunction}, {markupFunction}]
- */
-function determineSideModeRenderFunctions(modeSetting) {
-  if (SETTINGS_DATA_MAPPING[modeSetting] === 'WIW' && modeSetting === ACCEPTABLE_SETTINGS[0]) {
-    // set Labor
-    return [getWiwSchedule, markupLaborResults];
-  }
-  if (SETTINGS_DATA_MAPPING[modeSetting] === 'WIW') {
-    // set Events/labor
-    return [getWiwSchedule, markupEventShiftResults];
-  }
-  if (SETTINGS_DATA_MAPPING[modeSetting] === 'EMS') {
-    // set EMS events list
-    return [getEmsSchedule, markupEventListResults];
-  }
-  throw new Error('Mode setting error; unable to determine mode setting from env vars.');
-}
+// pull in utility functions
+const {
+  format,
+} = window.preload.utils;
 
 /** Infinite loop. Uses render mode flag to call relevant render functions and handles setting the
  * interval between when the data is refreshed. Will render less frequently in the case of errors
@@ -147,39 +43,28 @@ function handleReload(interval, initialInterval) {
   function errorBackOff(e) {
     console.error(e);
     handleReload(
-      Math.max(interval + initialInterval, parseInt(process.env.MAX_REFRESH_INTERVAL * 1000, 10)),
+      Math.min(interval + initialInterval, parseInt(MAX_REFRESH_INTERVAL * 1000, 10)),
       initialInterval,
     );
   }
 
   setTimeout(() => {
-    if (renderModeSingle) {
-      // don't have to consider which side is which -- left setting takes precedence
-      const renderFunctions = determineSideModeRenderFunctions(renderModeSettingLeft);
-      renderDataHtmlSingle(renderFunctions[0], renderFunctions[1]).then((result) => {
-        target.innerHTML = result;
-        handleReload(initialInterval, initialInterval);
-      })
-        .catch((e) => errorBackOff(e));
-    } else {
-      // Dual Render mode - one thing per side
-      const leftRenderFns = determineSideModeRenderFunctions(renderModeSettingLeft);
-      const rightRenderFns = determineSideModeRenderFunctions(renderModeSettingRight);
-      renderDataHtmlDual(leftRenderFns[0], leftRenderFns[1], rightRenderFns[0], rightRenderFns[1]).then((result) => {
-        target.innerHTML = result;
-        handleReload(initialInterval, initialInterval);
-      })
-        .catch((e) => errorBackOff(e));
-    }
+    // This call initiates a fetch to configured API server(s) in main.
+    updateApiContent().then(async (result) => {
+      // handle returned stuff from main
+      target.innerHTML = await result;
+      // successful update; reset reload interval to initial state
+      handleReload(initialInterval, initialInterval);
+    })
+      .catch((e) => errorBackOff(e));
   }, interval);
 }
-
 
 /**
  * Helper function that renders time data into HTML
  */
 function markupClock() {
-  return clockTemplate({ time: moment().format('dddd, MMMM Do YYYY, h:mm:ss a') });
+  return clockTemplate({ time: format(new Date(), 'EEEE, MMMM do yyyy, h:mm:ss a') });
 }
 
 /**
